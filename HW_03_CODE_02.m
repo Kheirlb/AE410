dec07ce382472be39563d2799b7035d24ca845c4
kheirlb <kheirlbparks@gmail.com>
2019-10-28 13:34:17 -0700

    first part of work

--- HW_03_CODE_02.m
+++ HW_03_CODE_02.m
@@ -1,10 +1,10 @@
-% HW_03_CODE_01.m
+% HW_03_CODE_02.m
 % Authors: Karl Parks and Jaden Bowyer
 % x = theta
 % v = theta_dot
 % w = omega
 
-clear; clc; close all;
+%clear; clc; close all;
 
 x_0 = (2/180)*pi;
 v_0 = -1/100;
@@ -16,20 +16,23 @@ t_f = (3*pi)/(2*w_n);
 t_f = 2*pi;
 t = linspace(t_0,t_f,1000);
 
-x = (sqrt(x_0 + (v_0/w_n)^2)) * sin(w_n*t + atan((x_0*w_n)/v_0));
+phi = atan((4*v_0)/(x_0*w_n*sqrt(15)) + 1/sqrt(15));
+X = x_0/(cos(phi));
+
+x = (exp((-w_n/4)*t) * X).*(cos(w_n*t*(sqrt(15)/4) + phi));
 fig1 = figure;
 plot(t,x)
 xlabel('$t$', 'Interpreter','latex');
 ylabel('$\theta(t)$', 'Interpreter','latex');
-title('Undamped Response');
+title('Underdamped Response');
 grid on;
 
-v = w_n*(sqrt(x_0 + (v_0/w_n)^2)) * cos(w_n*t + atan((x_0*w_n)/v_0));
+v = ((-w_n/4)*X*exp((-w_n/4)*t)).*(sqrt(15)*sin(w_n*t*(sqrt(15)/4) + phi) + cos(w_n*t*(sqrt(15)/4) + phi));
 fig2 = figure;
 plot(x,v)
 xlabel('$\theta(t)$', 'Interpreter','latex');
 ylabel('$\dot{\theta}(t)$', 'Interpreter','latex');
-title('Undamped Phase Plane');
+title('Underdamped Phase Plane');
 grid on;
 
 %hello jaden
---
+++ testPlot.m
@@ -0,0 +1,28 @@
+w_n = 10;
+
+t_0 = 0;
+t_f = (3*pi)/(2*w_n);
+t_f = 2*pi;
+t = linspace(t_0,t_f,1000);
+
+HW_03_CODE_01;
+
+x1 = x;
+v1 = v;
+HW_03_CODE_02;
+x2 = x;
+v2 = v;
+HW_03_CODE_03;
+x3 = x;
+v3 = v;
+HW_03_CODE_04;
+x4 = x;
+v4 = v;
+
+close all;
+%plot(x1,v1);
+hold on;
+plot(x2,v2);
+plot(x3,v3);
+plot(x4,v4);
+legend('Under','1','Over');
